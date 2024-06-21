a6b6a307f8d960d2e1748d46461dcd08193d2ae1
Frank Cangialosi <frank@breezeml.ai>
2024-06-21 10:53:27 -0400

    script to grab the latest activity

---
+++ get.py
@@ -0,0 +1,124 @@
+import datetime
+import json
+import logging
+import os
+import sys
+from getpass import getpass
+
+import requests
+from garth.exc import GarthHTTPError
+
+from garminconnect import (
+    Garmin,
+    GarminConnectAuthenticationError,
+    GarminConnectConnectionError,
+    GarminConnectTooManyRequestsError,
+)
+
+# Configure debug logging
+# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Load environment variables if defined
+with open(os.path.expanduser('~/.garminconnect/credentials.json'), 'r') as f:
+    credentials = json.loads(f.read())
+    email, password = credentials['email'], credentials['password']
+tokenstore = os.getenv("GARMINTOKENS") or "~/.garminconnect"
+api = None
+
+# Example selections and settings
+today = datetime.date.today()
+startdate = today - datetime.timedelta(days=7)  # Select past week
+start = 0
+limit = 100
+start_badge = 1  # Badge related calls calls start counting at 1
+activitytype = ""  # Possible values are: cycling, running, swimming, multi_sport, fitness_equipment, hiking, walking, other
+activityfile = "MY_ACTIVITY.fit"  # Supported file types are: .fit .gpx .tcx
+weight = 89.6
+weightunit = 'kg'
+
+def display_json(api_call, output):
+    """Format API output for better readability."""
+
+    dashed = "-" * 20
+    header = f"{dashed} {api_call} {dashed}"
+    footer = "-" * len(header)
+
+    print(header)
+
+    if isinstance(output, (int, str, dict, list)):
+        print(json.dumps(output, indent=4))
+    else:
+        print(output)
+
+    print(footer)
+
+def display_text(output):
+    """Format API output for better readability."""
+
+    dashed = "-" * 60
+    header = f"{dashed}"
+    footer = "-" * len(header)
+
+    print(header)
+    print(json.dumps(output, indent=4))
+    print(footer)
+
+
+def get_credentials():
+    """Get user credentials."""
+
+    email = input("Login e-mail: ")
+    password = getpass("Enter password: ")
+
+    return email, password
+
+
+def init_api(email, password):
+    """Initialize Garmin API with your credentials."""
+
+    try:
+        print(
+            f"Trying to login to Garmin Connect using token data from '{tokenstore}'...\n"
+        )
+        garmin = Garmin()
+        garmin.login(tokenstore)
+    except (FileNotFoundError, GarthHTTPError, GarminConnectAuthenticationError):
+        # Session is expired. You'll need to log in again
+        print(
+            "Login tokens not present, login with your Garmin Connect credentials to generate them.\n"
+            f"They will be stored in '{tokenstore}' for future use.\n"
+        )
+        try:
+            # Ask for credentials if not set as environment variables
+            if not email or not password:
+                email, password = get_credentials()
+
+            garmin = Garmin(email, password)
+            garmin.login()
+            # Save tokens for next login
+            garmin.garth.dump(tokenstore)
+
+        except (FileNotFoundError, GarthHTTPError, GarminConnectAuthenticationError, requests.exceptions.HTTPError) as err:
+            logger.error(err)
+            return None
+
+    return garmin
+
+api = init_api(email, password)
+
+last = api.get_last_activity()
+last_id = last['activityId']
+print(f"found activity with id {last_id}")
+tcx_data = api.download_activity(last_id, api.ActivityDownloadFormat.TCX)
+date = datetime.datetime.today().strftime("%m%d%y")
+out = f"treadmill-{date}.tcx"
+with open(out, "wb") as fb:
+    fb.write(tcx_data)
+print(f"tcx written to {out}")
+
+out = f"laps-{date}.txt"
+print(f"laps written to {out}")
+with open(out, "w") as fb:
+    fb.write("1")
